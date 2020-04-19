322c338a0af6c28e9bc92c2abc90abe21e18a1a8
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-04-19 20:34:42 -0300

    Added another origin to security request.

--- src/index.js
+++ src/index.js
@@ -18,9 +18,7 @@ const {
 app.use(cors());
 app.use(express.json());
 
-// io.origins(['http://localhost:3000', 'https://group-chatting.netlify.app/']);
-io.origins(['http://localhost:3000']);
-
+io.origins(['http://localhost:3000', 'https://group-chatting.netlify.app/']);
 io.on('connection', (socket) => {
   function disconnectUser() {
     const user = removeUser(socket.id);
