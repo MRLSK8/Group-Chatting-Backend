771a185c1ce2ec813d8201f3239ecc86659d3d5a
Marcelo Lima <jmdl.k8@gmail.com>
2021-04-17 13:22:09 -0300

    feat: chages api's info

--- src/router.js
+++ src/router.js
@@ -2,7 +2,14 @@ const { Router } = require('express');
 const routes = Router();
 
 routes.get('/', (req, res) => {
-  res.send('Server running...');
+  res.json({ 
+    author: 'Marcelo Lima',
+    github: 'https://github.com/MRLSK8',
+    lastUpdate: '17/04/2021',
+    Project: 'Group Chatting API',
+    Version: '1.0.0',
+    Status: 'Online',
+  });
 });
 
 module.exports = routes;
